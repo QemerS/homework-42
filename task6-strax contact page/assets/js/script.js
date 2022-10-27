f43fa09ac27bf410453eef911ef3f9e25891fca4
QemerS <tu201806169@code.edu.az>
2022-10-27 23:15:08 +0400

    mobile navbar done

--- task6-strax contact page/assets/js/script.js
+++ task6-strax contact page/assets/js/script.js
@@ -15,4 +15,22 @@ const dotBtn = document.querySelector('#dot-menu');
 const addition = document.querySelector('.additional-buttons');
 dotBtn.addEventListener('click', function () {
     addition.classList.toggle('additional-buttons-active')
-})
+});
+
+// giving position's top and left for overlay on hovering additiional buttons
+const additionsButtons = [...addition.children];
+additionsButtons.forEach(btn => {
+    btn.addEventListener('mouseenter', (e) => {
+        const overlay = e.target.children[2];
+        overlay.setAttribute('style', `left: ${e.offsetX}px; top: ${e.offsetY}px`);
+   })
+});
+
+const navLinksWithInnerMenu = [...document.getElementsByClassName('have-inner-links')];
+navLinksWithInnerMenu.forEach(mLinks => {
+    mLinks.addEventListener('click', function (e) {
+        e.preventDefault();
+        let innerLinks = this.nextElementSibling;
+        innerLinks.classList.toggle('inner-links-active');
+    })
+});
